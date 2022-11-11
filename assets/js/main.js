00b459171bf4b837e35264d355cee04d06380ee0
Daniel <angelicodaniel93@gmail.com>
2022-11-11 15:02:51 +0100

    inserimento immagine non dinamica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 var app = new Vue({
     el: '#root',
     data: {
-      
+      images: [ 'assets/img/Th06cover.jpg', 'assets/img/Th07cover.jpg', 'assets/img/Th08cover.jpg', 'assets/img/Th09cover.jpg', 'assets/img/Th10cover.jpg' ],
+      img: 0
     },
     methods: {
-  
+        
     }
   })
